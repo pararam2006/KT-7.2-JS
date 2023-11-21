baffb80afb040b599e180e680d8990a3a1fa1142
pararam2006 <95570198+pararam2006@users.noreply.github.com>
2023-11-21 11:58:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+let products = [
+    {
+        id: 1,
+        title: 'велосипед',
+        price: 45000,
+        count: 3, // количество на складе
+        marks: [4, 3, 5, 3] // оценки товара от 4х покупателей
+    },
+    {
+        id: 2,
+        title: 'ролики',
+        price: 25000,
+        count: 5,
+        marks: [4, 3, 5, 5]
+    },
+    {
+        id: 3,
+        title: 'арбалет',
+        price: 1700,
+        count: 9,
+        marks: [3, 3, 4, 5]
+    },
+    {
+        id: 4,
+        title: 'коньки',
+        price: 4500,
+        count: 3,
+        marks: [4, 3, 4, 3]
+    },
+    {
+        id: 5,
+        title: 'ракетки',
+        price: 900,
+        count: 15,
+        marks: [5, 3, 5, 3]
+    },
+    {
+        id: 6,
+        title: 'штанги',
+        price: 14000,
+        count: 5,
+        marks: [3, 3, 3, 2]
+    },
+    {
+        id: 7,
+        title: 'стрелы',
+        price: 1200,
+        count: 55,
+        marks: [3, 2, 4, 5]
+    },
+    {
+        id: 8,
+        title: 'мячи',
+        price: 500,
+        count: 49,
+        marks: [5, 4, 4, 4]
+    },
+    {
+        id: 9,
+        title: 'сетка',
+        price: 5000,
+        count: 6,
+        marks: [4, 5, 2, 5]
+    },
+    {
+        id: 10,
+        title: 'гантели',
+        price: 3400,
+        count: 12,
+        marks: [3, 2, 4, 2]
+    },
+    {
+        id: 11,
+        title: 'маты',
+        price: 16500,
+        count: 7,
+        marks: [4, 4, 4, 5]
+    }
+  ]
+    //ПУНКТ 1
+    let results1 = products.filter(item => item.count > 10);
+    console.log(`Count товаров > 10:`);
+    console.log(results1)
+    console.log()
+    //ПУНКТ 2
+    let results2 = products.find(item => item.price > 800 && item.price <= 900);
+    console.log(`Товар стоимостью от 800 до 900 включительно:`)
+    console.log(results2)
+    //ПУНКТ 3
+    function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a == b) return 0;
+        if (a < b) return -1;
+      }
+    let results3 = products.sort(compareNumeric)
+    console.log('Сортировка товаров по цене:')
+    console.log(results3)
+    //ПУНКТ 4
+    let results4 = products.reduce((sum, item) => sum + item.price, 0);
+    console.log(`Общая стоимость товаров: ${results4}`)
+    //ПУНКТ 5
+    console.log('marks_total:')
+    let updatedProducts = products.map(product => {
+      let marks_total = product.marks.reduce((total, mark) => total + mark, 0);
+      delete product.marks
+      return {...product, marks_total};
+    });
+    console.log(updatedProducts);
+
+
